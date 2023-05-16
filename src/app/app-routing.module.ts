e3c8649b9b807512a1d4a5322064bdd831ba0257
m-saleh96 <scyllies@gmail.com>
2023-05-16 11:14:51 +0300

    register Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,6 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-    // canActivate:[authGuardGuard]
   },
   {
     path: 'register',
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup , FormControl , Validators } from "@angular/forms";
 
 
 @Component({
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm : FormGroup;
+
+  constructor(){
+    this.registerForm = new FormGroup({
+      name : new FormControl(null, [Validators.required]),
+      email : new FormControl(null, [Validators.required , Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]),
+      username : new FormControl (null , [Validators.required, Validators.pattern(/^\S+$/)]),
+      password : new FormControl(null, [Validators.required , Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+~`\-={}[\]:;\"',.<>?/\\])(?!.*\s).{8,}$/)]),
+      confirmPassword : new FormControl(null, Validators.required)
+    })
+  }
+
+
+  submitRegisterForm(){
+    console.log(this.registerForm);
+    
+  }
+
+
+    
 
 }
