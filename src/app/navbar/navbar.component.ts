225bddb98bfd428025b0a41366c1564d5e64429c
m-saleh96 <scyllies@gmail.com>
2023-05-18 18:48:04 +0300

    connet with database api login and register

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CounterService } from '../services/counter.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +9,21 @@ import { CounterService } from '../services/counter.service';
 })
 export class NavbarComponent {
   counter:number = 0;
-  constructor(private counterservice : CounterService){}
+  isLogin:boolean = false ;
+  constructor(private counterservice : CounterService , private _AuthService:AuthService){
+    _AuthService.currentUsers.subscribe((data)=>{
+      if (data !=null) {
+        this.isLogin = true; 
+      } else {
+        this.isLogin = false;
+      }
+    })
+    
+  }
+  logOut(){
+    this._AuthService.logOut();
+  }
+  
   ngOnInit(){
     this.counterservice.counterVal.subscribe(res=>this.counter = res)
   }
